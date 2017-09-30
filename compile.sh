1303272d438c355eb6c8f05e0e90be803ef6e926
Sarah Bednar <shb66@pitt.edu>
2017-09-30 02:02:17 -0400

    script files added

---
+++ compile.sh
@@ -0,0 +1,1 @@
+javac -cp ".;./junit-4.12.jar;./hamcrest-core-1.3.jar" *.java
--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-javac -cp ".;./junit-4.12.jar;./hamcrest-core-1.3.jar" *.java
 java -cp ".;./junit-4.12.jar;./hamcrest-core-1.3.jar" TestRunner
